7ee7f9bb409ff1c67b767a19ef56590c0da44d2d
spjyotiranjan <jyotiranjan.sahoo@kalvium.community>
2023-11-21 18:50:26 +0530

    Done

--- promise-async.js
+++ promise-async.js
@@ -3,6 +3,7 @@ const newCookie = {name:"Biscotti Cookies"};
 
 // Progression 1: create a function to get all the cookies
 
+
    // Progression 2: using setTimeout() - use 1000 units for time parameter
 
 
@@ -15,7 +16,63 @@ const newCookie = {name:"Biscotti Cookies"};
       // Progression 5: handling errors and adding new cookie to the list
 
 
+      
+      
+      
+//----------------------------------------------------------------------------------------------------------------------------------//
+
+// Progression 1: create a function to get all the cookies
+function getCookies() {
+   // Progression 2: using setTimeout() -- use 1000 units for time parameter
+   setTimeout(() => {
+     let output = ""
+     cookies.forEach((e)=>{
+        output += `<li>${e.name}</li>`
+    })
+    document.body.innerHTML = output
+  }, 1000);
+}
+getCookies()
+
+//Progression 3: Create a function to creat cookies and create an object of Promise.
+function createCookie(newCookie) {
+   return new Promise((resolve,reject)=>{
+    // Progression 4: adding two parameters and using setTimeout() -- use 2000 units for time parameter
+    setTimeout(() => {
+      // Progression 5: handling errors and adding new cookie to the list
+      let initialLenOfCookie = cookies.length
+      cookies.push(newCookie)
+      
+      if (cookies.length >initialLenOfCookie) {
+         resolve("OK")
+      } else {
+         reject("There is no Cookie")
+      }
+   }, 2000);
+})
+}
+
+// Progression 6: call function using `.then`
+// createCookie(newCookie)
+// .then((res)=> {
+//    console.log(res);
+//    getCookies()
+// })
+// .catch(err=> console.log(err))      
+
+
 // Progression 7: creating a new async function
 
-// calling the new async function
+async function AsyncFunction(){
+   try{
+      let data = await createCookie(newCookie)
+      if(data == "OK"){
+         getCookies()
+      }
+   }catch(err){
+      console.error(err);
+   }
+}
 
+// calling the new async function
+AsyncFunction()
--- promise.js
+++ promise.js
@@ -2,14 +2,42 @@ const cookies = [{name:"Chocolate Cookies"},{name:"Macaron Cookies"}]
 const newCookie = {name:"Biscotti Cookies"};
 
 // Progression 1: create a function to get all the cookies
+function getCookies() {
   // Progression 2: using setTimeout() -- use 1000 units for time parameter
+  setTimeout(() => {
+    let output = ""
+    cookies.forEach((e)=>{
+      output += `<li>${e.name}</li>`
+    })
+    document.body.innerHTML = output
+  }, 1000);
+}
+getCookies()
 
 //Progression 3: Create a function to creat cookies and create an object of Promise.
-
+function createCookie(newCookie) {
+  return new Promise((resolve,reject)=>{
     // Progression 4: adding two parameters and using setTimeout() -- use 2000 units for time parameter
-      
+    setTimeout(() => {
       // Progression 5: handling errors and adding new cookie to the list
+      let initialLenOfCookie = cookies.length
+      cookies.push(newCookie)
+
+      if (cookies.length >initialLenOfCookie) {
+        resolve("OK")
+      } else {
+        reject("There is no Cookie")
+      }
+    }, 2000);
+  })
+}
 
 // Progression 6: call function using `.then`
+createCookie(newCookie)
+.then((res)=> {
+  getCookies()
+})
+.catch(err=> console.log(err))      
+
 
 
