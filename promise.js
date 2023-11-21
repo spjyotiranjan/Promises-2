ad30e6a7509443471da325f1fbe4f97c7138b862
spjyotiranjan <142651812+spjyotiranjan@users.noreply.github.com>
2023-11-21 18:20:05 +0530

    Initial commit

---
+++ promise.js
@@ -0,0 +1,15 @@
+const cookies = [{name:"Chocolate Cookies"},{name:"Macaron Cookies"}]
+const newCookie = {name:"Biscotti Cookies"};
+
+// Progression 1: create a function to get all the cookies
+  // Progression 2: using setTimeout() -- use 1000 units for time parameter
+
+//Progression 3: Create a function to creat cookies and create an object of Promise.
+
+    // Progression 4: adding two parameters and using setTimeout() -- use 2000 units for time parameter
+      
+      // Progression 5: handling errors and adding new cookie to the list
+
+// Progression 6: call function using `.then`
+
+
